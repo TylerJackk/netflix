c5aeaa0b22fbdbd5ebfe22b433c4784f407c3594
TylerJackk <liwep.x@gmail.com>
2021-03-21 19:54:57 +0800

    scrape add log output

--- scrape.py
+++ scrape.py
@@ -1,5 +1,8 @@
 import requests
 
+"""
+nohup python -u scrape.py > output.log &
+"""
 RESOURCE_TYPES = ["series", "movie"]
 
 
@@ -9,10 +12,17 @@ def scrape():
             f"https://w7lpq44qz8.execute-api.ap-east-1.amazonaws.com/api/{resource_type}/total/"
         )
         total = resp.json()["total"]
+        print(f'=====Start crawling {resource_type}=====')
+        print(f'total count: {total}')
         for offset in range(total // 100 + 1):
+            print(offset)
             resp = requests.get(
-                f"https://w7lpq44qz8.execute-api.ap-east-1.amazonaws.com/api/explore?offset={offset}&resource_type={resource_type}"
+                f"https://w7lpq44qz8.execute-api.ap-east-1.amazonaws.com/api/explore?offset={offset * 100}&resource_type={resource_type}"
             )
+            if resp.json()['success']:
+                print(f'offset {offset} explore triggered')
+            import time
+            time.sleep(60)
 
 
 if __name__ == "__main__":
