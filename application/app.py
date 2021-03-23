73f2eedfb0578d80bda18fd88d51f862151ec24d
TylerJackk <liwep.x@gmail.com>
2021-03-23 11:10:56 +0800

    send notification after scrape data

--- application/app.py
+++ application/app.py
@@ -1,6 +1,7 @@
 import json
 from chalice import Chalice
 
+from chalicelib.helper.send_notification import send_ifttt
 from chalicelib.db_manager.constants import NF_ID_QUEUE
 from chalicelib.db_manager.s3_client import S3Client
 from chalicelib.scraper.scraper import UnogsExplorer, UnogsScraper
@@ -15,13 +16,16 @@ def scrape_nf_detail(event):
         body = json.loads(record.body)
         nf_ids = body.get("nf_ids")
         resource_type = body.get("resource_type")
+        batch = body.get("batch")
         s3_data = {}
         for nf_id in nf_ids:
             scraper = UnogsScraper(nf_id)
             s3_data["nf_id"] = scraper.get_data()
         s3 = S3Client()
-        key = s3.build_key(resource_type, f'data-{body.get("batch")}')
+        key = s3.build_key(resource_type, f'data-{batch}')
         s3.put(key, s3_data)
+        message = f'{resource_type} data(batch {batch}) saved to S3'
+        send_ifttt(message)
 
 
 @app.route("/explore", methods=["GET"])
---
+++ application/chalicelib/helper/send_notification.py
@@ -0,0 +1,9 @@
+import requests
+
+EVENT = "NF Notification"
+
+
+def send_ifttt(msg, event=EVENT):
+    url = f"https://maker.ifttt.com/trigger/{event}/with/key/bDL5IbC1VbAsSr2xWvuTLC"
+    data = {'value1': msg}
+    requests.post(url, data=data)
