b05e1fa01f1e1a382a1db6f42104d27a8e6527c2
TylerJackk <liwep.x@gmail.com>
2021-04-13 20:16:03 +0800

    add etl handler sqs msg

--- application/app.py
+++ application/app.py
@@ -1,8 +1,9 @@
 import json
 from chalice import Chalice, Rate
 
+from chalicelib.etl.converter import do_etl
 from chalicelib.helper.send_notification import send_ifttt
-from chalicelib.db_manager.constants import NF_ID_QUEUE
+from chalicelib.db_manager.constants import NF_ID_QUEUE, NF_ETL_QUEUE
 from chalicelib.db_manager.s3_client import S3Client
 from chalicelib.scraper.constants import MOVIE, TV
 from chalicelib.scraper.scraper import UnogsExplorer, UnogsScraper
@@ -13,6 +14,9 @@ app.debug = True
 
 @app.on_sqs_message(queue=NF_ID_QUEUE, batch_size=1)
 def scrape_nf_detail(event):
+    """
+    triggered by sqs event, scrape detail info by netflix id
+    """
     for record in event:
         body = json.loads(record.body)
         nf_ids = body.get("nf_ids")
@@ -33,6 +37,9 @@ def scrape_nf_detail(event):
 
 @app.route("/explore", methods=["GET"])
 def explore_all_nf_data():
+    """
+    scrape all netflix data with pagination
+    """
     request = app.current_request
     resource_type = request.query_params["resource_type"]
     offset = int(request.query_params["offset"])
@@ -49,7 +56,25 @@ def get_resource_total(resource_type):
 
 
 @app.schedule(Rate(1, unit=Rate.DAYS))
-def explore_daily_new_resource(event):
+def explore_daily_new_resource():
     for resource_type in [TV, MOVIE]:
         explorer = UnogsExplorer(resource_type)
         explorer.search_new_resource()
+
+
+@app.on_sqs_message(queue=NF_ETL_QUEUE, batch_size=1)
+def etl(event):
+    """
+    triggered by sqs event, convert S3 raw data and load to ES
+    """
+    # a = {
+    #     "s3_paths": ['2021-03-23/data-0.json'],
+    #     "resource_type": 'movie'
+    # }
+    for record in event:
+        body = json.loads(record.body)
+        resource_type = body.get('resource_type')
+        s3_paths = body.get('s3_paths')
+        for s3_path in s3_paths:
+            s3_key = f'{resource_type}/{s3_path}'
+            do_etl(resource_type, s3_key)
