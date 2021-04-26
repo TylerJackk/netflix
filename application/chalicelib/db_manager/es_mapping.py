32cf6fcb86a5908f7659a091aa0e501022b76185
TylerJackk <liwep.x@gmail.com>
2021-04-26 16:16:44 +0800

    define more detail es mapping nested info

--- application/chalicelib/db_manager/es_mapping.py
+++ application/chalicelib/db_manager/es_mapping.py
@@ -18,7 +18,14 @@ ES_MAPPING = {
                 "actors": {"type": "text"},
                 "creators": {"type": "text"},
                 "images": {"type": "object"},
-                "country_info": {"type": "nested"},
+                "country_info": {
+                    "type": "nested",
+                    "properties": {
+                        "country_code": {"type": "keyword"},
+                        "audio": {"type": "keyword"},
+                        "subtitle": {"type": "keyword"},
+                    },
+                },
                 "created_time": {"type": "date"},
             },
         }
@@ -42,7 +49,14 @@ ES_MAPPING = {
                 "actors": {"type": "text"},
                 "creators": {"type": "text"},
                 "images": {"type": "object"},
-                "country_info": {"type": "nested"},
+                "country_info": {
+                    "type": "nested",
+                    "properties": {
+                        "country_code": {"type": "keyword"},
+                        "audio": {"type": "keyword"},
+                        "subtitle": {"type": "keyword"},
+                    },
+                },
                 "season_info": {"type": "nested"},
                 "created_time": {"type": "date"},
             },
